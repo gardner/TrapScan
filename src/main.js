80f4c85e7ab747e3cec7d0df0004e1924fa35647
Dylan Hobbs <dylan@dylanhobbs.ie>
2021-04-11 00:43:09 +0300

    Better guard

--- src/main.js
+++ src/main.js
@@ -8,6 +8,21 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.authRequired)
+  if (requireAuth && !currentUser) next('/')
+  else if (!requireAuth && currentUser) {
+    store.commit('setUser', { user: currentUser })
+    next('/dashboard')
+  } else if (currentUser) {
+    store.commit('setUser', { user: currentUser })
+    next()
+  } else {
+    next()
+  }
+})
+
 var firebaseConfig = {
   apiKey: 'AIzaSyDLnhlwQ2lA2246I6alj2GM1kbrYIIB_mo',
   authDomain: 'trapscan-app.firebaseapp.com',
@@ -20,9 +35,16 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+store.dispatch('auth')
+
+let app = ''
+firebase.auth().onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      vuetify,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
