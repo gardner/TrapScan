6cf91f3cf656ac6c043d15d03bee54fa61b0b827
gardner <gardner@bickford.nz>
2021-05-28 22:53:56 +0000

    Deploying to gh-pages from @ gardner/TrapScan@2a32fa8d1b03b749b2a86ec539e3dc5526addcc5 🚀

---
+++ precache-manifest.0b99492f8e893edae3c83dbe1efc39e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5151969856080fc122af",
+    "url": "/css/app.e44a10b8.css"
+  },
+  {
+    "revision": "32bc0ed3d208bafe23f7",
+    "url": "/css/chunk-vendors.a3beaac2.css"
+  },
+  {
+    "revision": "190cef181a80d830f995ad4d8a3865b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa1c2843ee70e7c89cb2",
+    "url": "/js/about.e478c824.js"
+  },
+  {
+    "revision": "5151969856080fc122af",
+    "url": "/js/app.1f3846b9.js"
+  },
+  {
+    "revision": "32bc0ed3d208bafe23f7",
+    "url": "/js/chunk-vendors.d2475c82.js"
+  },
+  {
+    "revision": "6f3ee115844d612dfac1709b5f08c68f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
